0c3347db9c97a87e0c706cd6f6e68f9b63c2c5f3
Imagineee.inc <imagineeelego@gmail.com>
2022-03-10 19:37:29 +0000

    added base export class

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const { defineConfig } = require('vite')
 
 module.exports = defineConfig({
-	root: "./example",
+	root: "./lib/example",
 	dest: "./dist",
   build: {
 		outDir: "../dist",
